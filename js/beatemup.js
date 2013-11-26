c63fc4e2cfbdf673285c729d162067ee39fd4f89
simbobwe@gmail.com <simbobwe@gmail.com@5e6de20a-4c61-2284-7c2b-127ccd3cbcbc>
2013-11-26 12:04:08 +0000

    replacing config constants and some class properties with attributes from config object

--- js/beatemup.js
+++ js/beatemup.js
@@ -1,32 +1,52 @@
 var canvas, game, player, enemy;
 
-var ARENA_WIDTH = 800;
-var ARENA_HEIGHT = 508;
-var ARENA_X = 0;
-var ARENA_Y = 0;
-var FIGHTER_WIDTH = 250;
-var FIGHTER_HEIGHT = 320;
-var FIGHTER_X = 0;
-var FIGHTER_Y = 160;
-var ENERGY_BAR_WIDTH = 324;
-var ENERGY_BAR_HEIGHT = 324;
-var ENERGY_BAR_X = 40;
-var ENERGY_BAR_Y = 60;
-var ENERGY_WIDTH = 320;
-var ENERGY_HEIGHT = 20;
-var ENERGY_X = 42;
-var ENERGY_Y = 62;
-var FIGHTER_NAME_WIDTH = 125;
-var FIGHTER_NAME_HEIGHT = 19;
-var FIGHTER_NAME_X = 66;
-var FIGHTER_NAME_Y = 62;
-var FIGHTER_WINS_WIDTH = 300;
-var FIGHTER_WINS_HEIGHT = 36;
-var FIGHTER_WINS_X = 250;
-var FIGHTER_WINS_Y = 112;
-var KEY_SPACE = 32;
-var KEY_LEFT = 37;
-var KEY_RIGHT = 39;
+var config = {
+	arena: {
+		width: 800,
+		height: 508,
+		x: 0,
+		y: 0
+	},
+	fighter: {
+		width: 250,
+		height: 320,
+		x: 0,
+		y: 160,
+		stepLength: 40,
+		vulnerability: 0.5,
+		damage: 5,
+		speed: 100
+	},
+	fighterName: {
+		width: 125,
+		height: 19,
+		x: 66,
+		y: 62
+	},
+	fighterWins: {
+		width: 300,
+		height: 36,
+		x: 250,
+		y: 112
+	},
+	energyBar: {
+		width: 324,
+		height: 24,
+		x: 40,
+		y: 60
+	},
+	energy: {
+		width: 320,
+		height: 20,
+		x: 42,
+		y: 62
+	},
+	keys: {
+		attack: 32,
+		moveLeft: 37,
+		moveRight: 39
+	}
+};
 
 function Game() {
 	this.images = null;
@@ -66,13 +86,13 @@ function Game() {
 	
 	this.bindKeys = function(event) {
 		switch (event.which) {
-			case KEY_SPACE:
+			case config.keys.attack:
 				player.attack();
 				break;
-			case KEY_LEFT:
+			case config.keys.moveLeft:
 				player.moveLeft();
 				break;
-			case KEY_RIGHT:
+			case config.keys.moveRight:
 				player.moveRight();
 				break;
 		}
@@ -112,21 +132,20 @@ function Game() {
 	};
 	
 	this.draw = function() {
-		canvas.drawImage(this.images.arena, ARENA_X, ARENA_Y);
-		
-		canvas.drawImage(this.images.player, player.clip, 0, FIGHTER_WIDTH, FIGHTER_HEIGHT, player.position.x, player.position.y + FIGHTER_Y, FIGHTER_WIDTH, FIGHTER_HEIGHT);
-		canvas.drawImage(this.images.enemy, enemy.clip, 0, FIGHTER_WIDTH, FIGHTER_HEIGHT, enemy.position.x, enemy.position.y + FIGHTER_Y, FIGHTER_WIDTH, FIGHTER_HEIGHT);
-		canvas.drawImage(this.images.playerEnergyBar, ENERGY_BAR_X, ENERGY_BAR_Y);
-		canvas.drawImage(this.images.enemyEnergyBar, ARENA_WIDTH - ENERGY_BAR_WIDTH - ENERGY_BAR_X, ENERGY_BAR_Y);
-		canvas.drawImage(this.images.playerEnergy, ENERGY_X, ENERGY_Y, player.energy / 100 * ENERGY_WIDTH, ENERGY_HEIGHT);
-		canvas.drawImage(this.images.enemyEnergy, ARENA_WIDTH - ENERGY_WIDTH - ENERGY_X + (100 - enemy.energy) / 100 * ENERGY_WIDTH, ENERGY_Y, enemy.energy / 100 * ENERGY_WIDTH, ENERGY_HEIGHT);
-		canvas.drawImage(this.images.playerName, FIGHTER_NAME_X, FIGHTER_NAME_Y, FIGHTER_NAME_WIDTH, FIGHTER_NAME_HEIGHT);
-		canvas.drawImage(this.images.enemyName, ARENA_WIDTH - FIGHTER_NAME_WIDTH - FIGHTER_NAME_X, FIGHTER_NAME_Y, FIGHTER_NAME_WIDTH, FIGHTER_NAME_HEIGHT);
+		canvas.drawImage(this.images.arena, config.arena.x, config.arena.y);
+		canvas.drawImage(this.images.player, player.clip, 0, config.fighter.width, config.fighter.height, player.position.x, player.position.y + config.fighter.y, config.fighter.width, config.fighter.height);
+		canvas.drawImage(this.images.enemy, enemy.clip, 0, config.fighter.width, config.fighter.height, enemy.position.x, enemy.position.y + config.fighter.y, config.fighter.width, config.fighter.height);
+		canvas.drawImage(this.images.playerEnergyBar, config.energyBar.x, config.energyBar.y);
+		canvas.drawImage(this.images.enemyEnergyBar, config.arena.width - config.energyBar.width - config.energyBar.x, config.energyBar.y);
+		canvas.drawImage(this.images.playerEnergy, config.energy.x, config.energy.y, player.energy / 100 * config.energy.width, config.energy.height);
+		canvas.drawImage(this.images.enemyEnergy, config.arena.width - config.energy.width - config.energy.x + (100 - enemy.energy) / 100 * config.energy.width, config.energy.y, enemy.energy / 100 * config.energy.width, config.energy.height);
+		canvas.drawImage(this.images.playerName, config.fighterName.x, config.fighterName.y, config.fighterName.width, config.fighterName.height);
+		canvas.drawImage(this.images.enemyName, config.arena.width - config.fighterName.width - config.fighterName.x, config.fighterName.y, config.fighterName.width, config.fighterName.height);
 		
 		if (this.winner == player) {
-			canvas.drawImage(this.images.playerWins, FIGHTER_WINS_X, FIGHTER_WINS_Y);
+			canvas.drawImage(this.images.playerWins, config.fighterWins.x, config.fighterWins.y);
 		} else if (this.winner == enemy) {
-			canvas.drawImage(this.images.enemyWins, FIGHTER_WINS_X, FIGHTER_WINS_Y);
+			canvas.drawImage(this.images.enemyWins, config.fighterWins.x, config.fighterWins.y);
 		}
 	};
 	
@@ -137,10 +156,6 @@ function Fighter() {
 	this.position = null;
 	this.opponent = null;
 	this.energy = 100;
-	this.stepLength = 40;
-	this.vulnerability = 0.5;
-	this.damage = 5;
-	this.delay = 60;
 	this.clip = 0;
 	this.reset = null;
 	
@@ -150,26 +165,26 @@ function Fighter() {
 				this.clip = 0;
 				break;
 			case 'moving-left':
-				this.clip = 1 * FIGHTER_WIDTH;
+				this.clip = 1 * config.fighter.width;
 				break;
 			case 'moving-right':
-				this.clip = 2 * FIGHTER_WIDTH;
+				this.clip = 2 * config.fighter.width;
 				break;
 			case 'punching':
-				this.clip = 3 * FIGHTER_WIDTH;
+				this.clip = 3 * config.fighter.width;
 				break;
 			case 'kicking':
-				this.clip = 4 * FIGHTER_WIDTH;
+				this.clip = 4 * config.fighter.width;
 				break;
 			case 'hit':
-				this.clip = 5 * FIGHTER_WIDTH;
+				this.clip = 5 * config.fighter.width;
 				break;
 			case 'winning':
-				this.clip = 6 * FIGHTER_WIDTH;
+				this.clip = 6 * config.fighter.width;
 				clearTimeout(this.reset);
 				break;
 			case 'losing':
-				this.clip = 7 * FIGHTER_WIDTH;
+				this.clip = 7 * config.fighter.width;
 				clearTimeout(this.reset);
 				break;
 		}
@@ -178,7 +193,7 @@ function Fighter() {
 			var self = this;
 			this.reset = setTimeout(function() {
 				self.changeAction(null, false, draw);
-			}, self.delay);
+			}, config.fighter.speed);
 		}
 		
 		if (draw == true || draw == undefined) {
@@ -197,9 +212,9 @@ function Fighter() {
 		this.changeAction('moving-left');
 		
 		if (this.position.x < this.opponent.position.x) {
-			this.changePosition(Math.max(0, this.position.x - this.stepLength), 0);
+			this.changePosition(Math.max(0, this.position.x - config.fighter.stepLength), 0);
 		} else {
-			this.changePosition(Math.max(this.opponent.position.x + FIGHTER_WIDTH * this.vulnerability, this.position.x - this.stepLength), 0);
+			this.changePosition(Math.max(this.opponent.position.x + config.fighter.width * config.fighter.vulnerability, this.position.x - config.fighter.stepLength), 0);
 		}
 	};
 	
@@ -207,9 +222,9 @@ function Fighter() {
 		this.changeAction('moving-right');
 		
 		if (this.position.x < this.opponent.position.x) {
-			this.changePosition(Math.min(this.opponent.position.x - FIGHTER_WIDTH * this.vulnerability, this.position.x + this.stepLength), 0);
+			this.changePosition(Math.min(this.opponent.position.x - config.fighter.width * config.fighter.vulnerability, this.position.x + config.fighter.stepLength), 0);
 		} else {
-			this.changePosition(Math.min(this.position.x + this.stepLength, ARENA_WIDTH - FIGHTER_WIDTH), 0);
+			this.changePosition(Math.min(this.position.x + config.fighter.stepLength, config.arena.width - config.fighter.width), 0);
 		}
 	};
 	
@@ -222,8 +237,8 @@ function Fighter() {
 		
 		this.changeAction(attack);
 		
-		if ((this.position.x < this.opponent.position.x && (this.opponent.position.x - this.position.x) < FIGHTER_WIDTH) ||
-			(this.opponent.position.x < this.position.x && (this.position.x - this.opponent.position.x) < FIGHTER_WIDTH)) {
+		if ((this.position.x < this.opponent.position.x && (this.opponent.position.x - this.position.x) < config.fighter.width) ||
+			(this.opponent.position.x < this.position.x && (this.position.x - this.opponent.position.x) < config.fighter.width)) {
 			this.opponent.getHit();
 		}
 	};
@@ -231,7 +246,7 @@ function Fighter() {
 	this.getHit = function() {
 		this.changeAction('hit');
 		
-		this.energy = Math.max(0, this.energy - this.damage);
+		this.energy = Math.max(0, this.energy - config.fighter.damage);
 		
 		game.checkEnergy();
 	};
@@ -251,7 +266,7 @@ Player.prototype.constructor = Player;
 function Player() {
 	this.construct = function() {
 		this.changeAction(null, false, false);
-		this.changePosition(FIGHTER_X, 0);
+		this.changePosition(config.fighter.x, 0);
 	};
 	
 	this.construct();
@@ -265,7 +280,7 @@ function Enemy() {
 	
 	this.construct = function() {
 		this.changeAction(null, false, false);
-		this.changePosition(ARENA_WIDTH - FIGHTER_WIDTH - FIGHTER_X, 0);
+		this.changePosition(config.arena.width - config.fighter.width - config.fighter.x, 0);
 	};
 	
 	this.startFighting = function() {
@@ -277,7 +292,7 @@ function Enemy() {
 				self.moveRight();
 			}
 			
-			if (self.opponent.position.x >= (self.position.x - FIGHTER_WIDTH * self.vulnerability)) {
+			if (self.opponent.position.x >= (self.position.x - config.fighter.width * config.fighter.vulnerability)) {
 				self.attack();
 			}
 		}, 200);
